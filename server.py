cadcd3352101723ed8df26d067b64bf3113428ec
Soumya Pal <pal.soumya1996@gmail.com>
2021-05-18 19:14:36 -0400

    update

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect
-import requests
+import os
 import pandas as pd
 from web3 import Web3
 import json
 
 app = Flask(__name__, template_folder='templates')
+UPLOAD_FOLDER = os.path.abspath(os.path.join(os.getcwd(), "static"))
+ALLOWED_EXTENSIONS = set(['py'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 
@@ -16,6 +19,11 @@ with open("static/Market.json", 'r') as f:
 
 w3.eth.defaultAccount = w3.eth.accounts[1]
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+        
 @app.route('/')
 def hello():
     return render_template("index.html",title= 'Etheros')
@@ -25,24 +33,25 @@ def sensorAdd():
     if request.method == 'POST':
         # Create the contract instance with the newly-deployed address
         market = w3.eth.contract(address=contract_address, abi=abi)
-        body = request.get_json()
-        result1 = request.values['tag']
-        result2 = request.values['nameNdesc']
-        result3 = '0x29543BAea4aC8aA06f9c01F8aa1c00f4E3970E33'
-        print(result1)
-        print(result2)
-        _price = Web3.toWei('1.0', "ether");
+        #body = request.get_json()
+        tag = request.values['tag']
+        name = request.values['nameNdesc']
+        wallet_addr = request.values['walletaddr'] #'0x29543BAea4aC8aA06f9c01F8aa1c00f4E3970E33'
+        _price = Web3.toWei('1.0', "ether")
+        print(tag)
+        print(name)
         print(_price)
+
         #if error:
         #    return jsonify(error), 422
         tx_hash = market.functions.sellArticle(
-           result1, result2, _price
+           tag, name, _price
         )
-        tx_hash = tx_hash.transact({'from': result3})
+        tx_hash = tx_hash.transact({'from': wallet_addr})
         # Wait for transaction to be mined...
         w3.eth.waitForTransactionReceipt(tx_hash)
-        print(tx_hash)
-        user_data = market.events.LogSellArticle
+        #print(tx_hash)
+        #user_data = market.events.LogSellArticle
         #return jsonify({"data": user_data}), 200
         #print(user_data['_seller'])
         #print(user_data['_name'])
@@ -50,5 +59,19 @@ def sensorAdd():
     else:
         return render_template("sensorform.html") 
 
+@app.route("/fetchData", methods=['POST', 'GET'])
+def sensorAdd():
+    if request.method == 'POST':
+        #body = request.get_json()
+        timeStart = request.values['timeStart']
+        timeEnd = request.files['timeEnd']
+        uploaded_file = request.files['file']
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            file_ext = os.path.splitext(uploaded_file.filename)[1]
+            filesave = str('MachineLearning') + str(file_ext)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filesave))
+        print(timeStart)
+        print(timeEnd)
+    
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
