f7644a37d3d2c7ea89ec36b07186beb2eb01ea11
Soumya Pal <pal.soumya1996@gmail.com>
2021-05-19 11:10:21 -0400

    update

--- server.py
+++ server.py
@@ -72,6 +72,8 @@ def sensorAdd():
             uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filesave))
         print(timeStart)
         print(timeEnd)
+        return redirect('/')
+
     
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
